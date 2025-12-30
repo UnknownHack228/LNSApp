5edcc2687d020f097e7e917ea96db0fddf6a16f7
UnknownHack228 <shurgalinivan.aleks96@gmail.com>
2025-12-30 18:50:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from kivy.uix.label import Label
 
 class LNSApp(App):
     def build(self):
-        layout = BoxLayout(orientation='vertical', padding=30, spacing=20)
-        layout.add_widget(Label(text='🎵 LNS Music Controller', font_size=28))
-        layout.add_widget(Button(text='🔍 Scan Bluetooth', size_hint=(1, 0.2)))
-        layout.add_widget(Button(text='▶️ Play Music', size_hint=(1, 0.2)))
-        layout.add_widget(Button(text='🎤 Microphone to Speaker', size_hint=(1, 0.2)))
-        layout.add_widget(Button(text='⚙️ Settings', size_hint=(1, 0.2)))
-        return layout
+       layout = BoxLayout(orientation='vertical', padding=30, spacing=20)
+       layout.add_widget(Label(text='🎵 LNS Music Controller', font_size=28))
+       layout.add_widget(Button(text='🔍 Scan Bluetooth', size_hint=(1, 0.2)))
+       layout.add_widget(Button(text='▶️ Play Music', size_hint=(1, 0.2)))
+       layout.add_widget(Button(text='🎤 Microphone to Speaker', size_hint=(1, 0.2)))
+       layout.add_widget(Button(text='⚙️ Settings', size_hint=(1, 0.2)))
+       return layout
 
 if name == 'main':
     LNSApp().run()
